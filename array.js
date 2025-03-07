fb54bac50fddef425daf852dbe21b8b6ab6a62c6
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-03-07 12:38:34 +0600

    arguments function

---
+++ array.js
@@ -0,0 +1,7 @@
+const products = [
+    {name:'phone', price:23345},
+    {name:'laptop', price:20345},
+
+]
+console.log(Array.isArray)
+console.log (products[1])
---
+++ object.js
@@ -0,0 +1,18 @@
+const student = {
+    name: 'palash',
+    id:423,
+    address:"badda",
+    isSingle:true,
+    friends:['ahasan','habib', 'polash', 'shubastu'],
+    car:{
+        brand:'tesla',
+        price: 5000000,
+        made:2025,
+        manufracturer:{
+            name:'xlon' ,
+            founder:'elon mask' ,
+            country:'USA'
+        }
+    }
+}
+console.log(student.car)
